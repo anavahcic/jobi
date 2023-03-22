ab42e6b2a8b37a57e04a502e91cd3b7e5b5875b6
Ana Vahcic <anavahcic@Anas-MacBook-Air.local>
2023-03-21 23:05:40 -0600

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDj3GIGQRBZ-EyHWs2-a1vQSZ6pYz4GQ54",
+  authDomain: "jobi-4ec83.firebaseapp.com",
+  projectId: "jobi-4ec83",
+  storageBucket: "jobi-4ec83.appspot.com",
+  messagingSenderId: "1095392188377",
+  appId: "1:1095392188377:web:f6c0c237a4b29f9c692bc8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
